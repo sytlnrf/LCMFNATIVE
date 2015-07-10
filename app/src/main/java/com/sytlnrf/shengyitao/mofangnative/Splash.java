a0c8480ccb0a459220e289dc06ef34de89d55178
sytlnrf <sheng_yitao@163.com>
2015-07-10 23:43:52 +0800

    add splash

---
+++ app/src/main/java/com/sytlnrf/shengyitao/mofangnative/Splash.java
@@ -0,0 +1,29 @@
+package com.sytlnrf.shengyitao.mofangnative;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+
+public class Splash extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGHT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(Splash.this, MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+
+
+}
