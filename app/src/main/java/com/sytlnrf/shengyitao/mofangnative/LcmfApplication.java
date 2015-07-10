a55454eaadc7a089dc75f2a974bb25edfeb2bc61
sytlnrf <sheng_yitao@163.com>
2015-07-10 16:40:30 +0800

    create Application

---
+++ app/src/main/java/com/sytlnrf/shengyitao/mofangnative/LcmfApplication.java
@@ -0,0 +1,9 @@
+package com.sytlnrf.shengyitao.mofangnative;
+
+import android.app.Application;
+
+/**
+ * Created by shengyitao on 15/7/10.
+ */
+public class LcmfApplication extends Application {
+}
